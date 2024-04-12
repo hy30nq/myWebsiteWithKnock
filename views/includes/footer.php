a561fbccc00dfb4616d4fd09b0cc7e52546ebd21
techq <121498814+hyeonql@users.noreply.github.com>
2024-04-12 12:53:52 +0900

    Update footer.php

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,8 +1,8 @@
 <footer>
   <p>Copyright © 2024 All rights reserved</p>
   <div class="social-media">
-    <a href="#">Tistory</a>
-    <a href="#">facebook</a>
-    <a href="#">instagram</a>
+    <a href="https://hyeonql.tistory.com/" target="_blank">Tistory</a>
+    <a href="https://www.facebook.com/profile.php?id=100088166530950" target="_blank">facebook</a>
+    <a href="https://www.instagram.com/hy30nq/" target="_blank">instagram</a>
   </div>
 </footer>
