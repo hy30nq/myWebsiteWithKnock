210bd6a4f5bee9407ecef962aedd9c019b29a1ea
techq <121498814+hyeonql@users.noreply.github.com>
2024-04-13 21:36:41 +0900

    Update index.php
    모바일에서 접속시 alert 출력 추가

--- index.php
+++ index.php
@@ -13,6 +13,22 @@ session_start();
   <link rel="stylesheet" href="/public/styles/styles.css" />
   <link rel="stylesheet" href="/public/styles/index_style.css" />
 
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+        // 화면의 너비를 확인하여 모바일 디바이스인지 판단하는 함수
+        function checkIfMobile() {
+            const maxWidth = 800; // 모바일로 간주되는 최대 화면 너비
+            if (window.innerWidth <= maxWidth) {
+                alert('모바일 기기로 접속하셨습니다. 해당 사이트는 컴퓨터에 최적화 되어 있습니다. 컴퓨터 환경에서 접속하는 것을 추천합니다. 모바일은 화면 인터페이스가 제대로 보이지 않을 수 있습니다.');
+            }
+        }
+
+        // 함수 실행
+        checkIfMobile();
+    });
+</script>
+
+
 </head>
 
 <body>
